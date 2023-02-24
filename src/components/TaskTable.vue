12d59b5173af4292ed377a617066a248fa62c6a5
Kataev Evgeniy <jackman.bup@gmail.com>
2023-02-24 23:25:00 +0500

    change getting and creating tasks add creating column

--- src/components/TaskTable.vue
+++ src/components/TaskTable.vue
@@ -1,57 +1,78 @@
 <template>
     <div class="task_container">
-        <button @click="getAllTasks()"></button>
-        <div 
-            class="column" 
-            :key="i" 
-            v-for="(column, i) in array"
-            @drop="drop(column.tasks,i)" 
-            @dragover.prevent
-            >
-            <div
-                class="task" 
-                :key="j" 
-                v-for="(task,j) in column.tasks"
-                draggable="true" 
-                @dragstart="dragStart(task,column.tasks)"
-                @mouseover="showMenu('menu' + i + j)" @mouseleave="hideMenu('menu' + i + j)"
+        <button @click="getAllColumns();"></button>
+        
+        
+            <div 
+                
+                class="column" 
+                :key="i" 
+                v-for="(column, i) in columns"
+                
                 >
-                <div 
-                    
-                    >{{ task.text }}
-                </div>
-                <div :id="'menu' + i + j" style="display: none;">
-                    <button>delete</button>
-                    <button>change</button>
+                <div>{{ column.title }}</div>
+                <!-- <div
+                    @drop="drop(column.tasks,i)" 
+                    @dragover.prevent
+                    class="task" 
+                    :key="j" 
+                    v-for="(task,j) in column.tasks"
+                    draggable="true" 
+                    @dragstart="dragStart(task,column.tasks)"
+                    @mouseover="showMenu('menu' + i + j)" @mouseleave="hideMenu('menu' + i + j)"
+                    >
+                    <div>{{ task.text }}</div>
+                    <div :id="'menu' + i + j" style="display: none;">
+                        <button @click="deleteTask(task)">delete</button>
+                        <button>change</button>
+                    </div>
+                </div> -->
+                <button v-if="selectedColumn != i" @click="selectedColumn = i">Add Task</button>
+                <div v-if="selectedColumn == i">
+                    <textarea 
+                        oninput="this.style.height = (this.scrollHeight - 4) + 'px';"
+                        :value="taskTemplate.text" 
+                        placeholder="Название" 
+                        @input="taskTemplate.text = $event.target.value"  
+                        maxlength="500"
+                        class="task_template_text">
+                    </textarea>
+                    <button @click="taskTemplate.tableColumn = column; createTask(taskTemplate)">Add Task</button>
                 </div>
             </div>
-            <textarea 
-                oninput="this.style.height = (this.scrollHeight - 4) + 'px';"
-                :value="column.taskTemplate.text" 
-                placeholder="Название" 
-                @input="column.taskTemplate.text = $event.target.value"  
-                maxlength="500"
-                class="task_template_text">
-            </textarea>
-            <button @click="column.taskTemplate.columnNumber = i; createTask(column.taskTemplate)">Add Task</button>
+            <div>
+                <textarea 
+                    oninput="this.style.height = (this.scrollHeight - 4) + 'px';"
+                    :value="columnTemplate.title" 
+                    placeholder="Название" 
+                    @input="columnTemplate.title = $event.target.value"  
+                    maxlength="100"
+                    class="task_template_text">
+                </textarea><br>
+                <button @click="createColumn(columnTemplate)">Create column</button>
+            </div>
         </div>
-    </div>
+    
 </template>
   
 <script>
-    import axios from 'axios'
+
+import axios from 'axios'
     export default {
         data(){
             return{
-                array:[
-                    {tasks:[],taskTemplate:{columnNumber:null,text:""}},
-                    {tasks:[],taskTemplate:{columnNumber:null,text:""}}
-                ],
+                columns:[],
+                tasksMap:{},
                 select:{
                     task:null,
                     sourceColumn:[]
                 },
-                
+                taskTemplate:{
+                    tableColumn:null,
+                    text:""
+                },
+                columnTemplate:{title:""},
+                selectedColumn:null,
             }
         },
         methods: {
@@ -74,7 +95,56 @@
                 column.push(task);
                 this.updateTask(task);
             },
+            async createColumn(column){
+                await axios.post("http://localhost:8081/taskScheduler/saveTableColumn", column,{headers:{'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')}})
+                    .then((response) => {
+                        console.log(response);//TODO delete this
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                });
+            },
+            async updateColumn(column){
+                await axios.post("http://localhost:8081/taskScheduler/updateTableColumn", column,{headers:{'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')}})
+                    .then((response) => {
+                        console.log(response);//TODO delete this
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                });
+            },
+            async deleteColumn(column){
+                await axios.post("http://localhost:8081/taskScheduler/deleteTableColumn", column,{headers:{'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')}})
+                    .then((response) => {
+                        console.log(response);//TODO delete this
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                });
+            },
+            async getAllColumns(){
+                await axios.get("http://localhost:8081/taskScheduler/getAllTableColumns",{headers:{'Authorization': localStorage.getItem('token'),
+                    "Access-Control-Allow-Origin": "*"}})
+                    .then((response) => {
+                        console.log(response);
+                        this.columns = response.data
+                        this.columns.forEach((column,index)=>{
+                            this.getAllTasks(column,index);
+                        })
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    
+                });
+            },
             async createTask(data){
+                if(data.text === ""){
+                    this.$store.dispatch('showMessage',{messageText:'Cannot create empty task',color:'red'})
+                    return;
+                }
                 await axios.post("http://localhost:8081/taskScheduler/saveTableTask", data,{headers:{'Content-Type': 'application/json',
                 'Authorization': localStorage.getItem('token')}})
                     .then((response) => {
@@ -95,25 +165,24 @@
                 });
                 
             },
-            async getAllTasks(){
-                await axios.get("http://localhost:8081/taskScheduler/getAllTableTasks",{headers:{'Authorization': localStorage.getItem('token'),
-                    "Access-Control-Allow-Origin": "*"}})
+            async deleteTask(data){
+                await axios.post("http://localhost:8081/taskScheduler/deleteTableTask", data,{headers:{'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')}})
+                    .then((response) => {
+                        console.log(response);//TODO delete this
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                });
+                
+            },
+            getAllTasks(column,index){
+                axios.post("http://localhost:8081/taskScheduler/getAllTableTasks",column,{headers:{'Authorization': localStorage.getItem('token'),
+                'Content-Type': 'application/json'}})
                     .then((response) => {
                         console.log(response);
-                        let arra = response.data
-                        this.array = []
-                        arra.forEach((task) => {
-                            if(this.array[task.columnNumber] == null){
-                                this.array[task.columnNumber] = {
-                                    tasks:[],
-                                    taskTemplate:{
-                                        columnNumber:null,
-                                        text:""
-                                    }
-                                }
-                            }
-                            this.array[task.columnNumber].tasks.push(task)
-                        });
+                        this.tasksMap[index] = response.data;
+                        
                     })
                     .catch(function (error) {
                         console.log(error);
